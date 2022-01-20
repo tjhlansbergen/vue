a38f08efb529bfe21b05194a7a460f1edff80834
Tako Lansbergen <tako@uptrends.com>
2022-01-20 16:15:11 +0100

    initial commit with 2 template projects

---
+++ single-file-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div>{{ myDataProperty }} &#8594; <input :value="myDataProperty" @input="updateMyProperty($event)"/></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+  // Data property
+  myDataProperty = 'update me!'
+
+  // Component method
+  updateMyProperty ($event: { target: { value: string } }): void {
+    this.myDataProperty = $event.target.value
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: whitesmoke;
+  background-color: tomato;
+  margin-top: 60px;
+}
+</style>
